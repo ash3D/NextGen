28e03271278c94cc948d7150bb6258e1776ba48a
ash3D <alexey.shaydurov@deeprosoft.com>
2016-02-11 12:22:03 +0500

    Minor refactoring.

--- General/rotate.h
+++ General/rotate.h
@@ -20,8 +20,10 @@ See "DGLE.h" for more details.
 
 namespace RotImpl
 {
+	using std::enable_if_t;
+
 	template<unsigned n>
-	auto rol(typename boost::uint_t<n>::fast value, unsigned int shift) noexcept -> std::enable_if_t<n != 8 && n != 16 && n != 32 && n != 64, decltype(value)>
+	auto rol(typename boost::uint_t<n>::fast value, unsigned int shift) noexcept -> enable_if_t<n != 8 && n != 16 && n != 32 && n != 64, decltype(value)>
 	{
 		shift %= n;
 		const auto mask = (decltype(value)(1) << n) - decltype(value)(1);
@@ -29,34 +31,36 @@ namespace RotImpl
 	}
 
 #ifdef _MSC_VER
+	using std::function;
+
 	template<unsigned n>
-	std::enable_if_t<n == 8, typename std::function<decltype(_rotl8)>::result_type> rol(
-		typename std::function<decltype(_rotl8)>::first_argument_type value,
-		typename std::function<decltype(_rotl8)>::second_argument_type shift)
+	enable_if_t<n == 8, typename function<decltype(_rotl8)>::result_type> rol(
+		typename function<decltype(_rotl8)>::first_argument_type value,
+		typename function<decltype(_rotl8)>::second_argument_type shift)
 	{
 		return _rotl8(value, shift);
 	}
 
 	template<unsigned n>
-	std::enable_if_t<n == 16, typename std::function<decltype(_rotl16)>::result_type> rol(
-		typename std::function<decltype(_rotl16)>::first_argument_type value,
-		typename std::function<decltype(_rotl16)>::second_argument_type shift)
+	enable_if_t<n == 16, typename function<decltype(_rotl16)>::result_type> rol(
+		typename function<decltype(_rotl16)>::first_argument_type value,
+		typename function<decltype(_rotl16)>::second_argument_type shift)
 	{
 		return _rotl16(value, shift);
 	}
 
 	template<unsigned n>
-	std::enable_if_t<n == 32, typename std::function<decltype(_rotl)>::result_type> rol(
-		typename std::function<decltype(_rotl)>::first_argument_type value,
-		typename std::function<decltype(_rotl)>::second_argument_type shift)
+	enable_if_t<n == 32, typename function<decltype(_rotl)>::result_type> rol(
+		typename function<decltype(_rotl)>::first_argument_type value,
+		typename function<decltype(_rotl)>::second_argument_type shift)
 	{
 		return _rotl(value, shift);
 	}
 
 	template<unsigned n>
-	std::enable_if_t<n == 64, typename std::function<decltype(_rotl64)>::result_type> rol(
-		typename std::function<decltype(_rotl64)>::first_argument_type value,
-		typename std::function<decltype(_rotl64)>::second_argument_type shift)
+	enable_if_t<n == 64, typename function<decltype(_rotl64)>::result_type> rol(
+		typename function<decltype(_rotl64)>::first_argument_type value,
+		typename function<decltype(_rotl64)>::second_argument_type shift)
 	{
 		return _rotl64(value, shift);
 	}
