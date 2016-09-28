88d1ba39f976bfc87cdd19b77b5c7f5283da7281
ash3D <alexey.shaydurov@deeprosoft.com>
2016-09-28 13:47:01 +0500

    Copy elimination in "swizzle op scalar'.

--- DGLE_HighLevel/General/vector math.h
+++ DGLE_HighLevel/General/vector math.h
@@ -1,6 +1,6 @@
 /**
 \author		Alexey Shaydurov aka ASH
-\date		27.09.2016 (c)Alexey Shaydurov
+\date		28.09.2016 (c)Alexey Shaydurov
 
 This file is a part of DGLE2 project and is distributed
 under the terms of the GNU Lesser General Public License.
@@ -2106,7 +2106,7 @@ further investigations needed, including other compilers
 					const RightType &right)																											\
 					{																																\
 						decltype(left op right) result(left);																						\
-						return result op##= right;																									\
+						return Workaround::operator op##=<false>(result, right);																	\
 					}
 				GENERATE_OPERATORS(OPERATOR_DEFINITION, ARITHMETIC_OPS)
 #				undef OPERATOR_DEFINITION
