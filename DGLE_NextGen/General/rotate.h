84f8a56f1f07af5b9a5b3f5360808946c03bd591
ash3D <alexey.shaydurov@deeprosoft.com>
2016-11-20 16:39:41 +0500

    Check for 0 bit rotate width added.

--- DGLE_NextGen/General/rotate.h
+++ DGLE_NextGen/General/rotate.h
@@ -225,6 +225,7 @@ namespace RotImpl
 	inline auto rot(Value value, Shift shift)
 	{
 		static_assert(is_integral_v<Value> && is_integral_v<Shift>, "rotate is feasible for integral types only");
+		static_assert(width > 0, "rotate width cannot be 0");
 		static_assert(width <= numeric_limits<uintmax_t>::digits, "too large rotate width");
 		return rot_dispatch<dir, width>(value, shift);
 	}
