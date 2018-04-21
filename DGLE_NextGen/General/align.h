631722cf26ac07c05ac5a8cf250b722484ca687a
ash3D <alexey.shaydurov@deeprosoft.com>
2018-04-21 18:59:15 +0500

    AlignSize() overload with runtime alignment.

--- DGLE_NextGen/General/align.h
+++ DGLE_NextGen/General/align.h
@@ -1,6 +1,6 @@
 /**
 \author		Alexey Shaydurov aka ASH
-\date		13.11.2017 (c)Korotkov Andrey
+\date		21.04.2018 (c)Korotkov Andrey
 
 This file is a part of DGLE project and is distributed
 under the terms of the GNU Lesser General Public License.
@@ -11,6 +11,7 @@ See "DGLE.h" for more details.
 
 #include "CompilerCheck.h"
 #include "popcnt.h"
+#include <cassert>
 #include <type_traits>
 
 template<unsigned alignment, typename UInt>
@@ -18,5 +19,13 @@ constexpr inline auto AlignSize(UInt x)
 {
 	static_assert(std::is_unsigned_v<UInt>, "alignment should be unsigned integral");
 	static_assert(popcnt<>(alignment) == 1, "alignemt should be power of 2");
+	return x + alignment - 1 & ~(alignment - 1);
+}
+
+template<typename UInt>
+inline auto AlignSize(UInt x, unsigned alignment)
+{
+	static_assert(std::is_unsigned_v<UInt>, "alignment should be unsigned integral");
+	assert(popcnt(alignment) == 1);
 	return x + alignment - 1 & ~(alignment - 1);
 }
