968ac5a0fb6c0a8f8c8564007a3be650a0b4664b
ash3D <alexey.shaydurov@deeprosoft.com>
2018-04-30 20:15:40 +0500

    AABB 2D vertex shader now uses shared per-frame data definition in place of custom CB definition.

--- DGLE_NextGen/Renderer/AABB_2D.hlsl
+++ DGLE_NextGen/Renderer/AABB_2D.hlsl
@@ -1,21 +1,17 @@
 /**
 \author		Alexey Shaydurov aka ASH
-\date		17.04.2018 (c)Korotkov Andrey
+\date		30.04.2018 (c)Korotkov Andrey
 
 This file is a part of DGLE project and is distributed
 under the terms of the GNU Lesser General Public License.
 See "DGLE.h" for more details.
 */
 
+#include "per-frame data.hlsli"
+
 struct AABB
 {
 	float2 min : AABB_min, max : AABB_max;
-};
-
-cbuffer Xforms : register(b0)
-{
-	row_major float4x4 projXform;
-	row_major float4x3 viewXform, worldXform;
 };
 
 /*
@@ -29,7 +25,7 @@ For larger batches GS approach can also lead to better results.
 float4 main(in uint quadVertexID : SV_VertexID, in AABB aabb) : SV_POSITION
 {
 	const float2 corner = lerp(aabb.min, aabb.max, uint2(quadVertexID & 1u, quadVertexID >> 1u));
-	const float3 worldPos = mul(float4(corner, 0.f, 1.f), worldXform);
+	const float3 worldPos = mul(float4(corner, 0.f, 1.f), terrainWorldXform);
 	const float3 viewPos = mul(float4(worldPos, 1.f), viewXform);
 	return mul(float4(viewPos, 1.f), projXform);
 }
