1b11701b067944e61fd533df822d2966d0f3dbce
ash3D <alexey.shaydurov@deeprosoft.com>
2018-06-27 02:28:29 +0500

    Typo fix.

--- DGLE_NextGen/Renderer/object3D_PS.hlsl
+++ DGLE_NextGen/Renderer/object3D_PS.hlsl
@@ -1,6 +1,6 @@
 /**
 \author		Alexey Shaydurov aka ASH
-\date		25.06.2018 (c)Korotkov Andrey
+\date		27.06.2018 (c)Korotkov Andrey
 
 This file is a part of DGLE project and is distributed
 under the terms of the GNU Lesser General Public License.
@@ -20,6 +20,6 @@ float4 main(in XformedVertex input) : SV_TARGET
 {
 	const float3 color = Lit(albedo, .5f, F0(1.55f), normalize(input.N), normalize(input.viewDir), sun.dir, sun.irradiance);
 
-	// biuilt-in simple tonemapping for now
+	// built-in simple tonemapping for now
 	return float4(color / (color + 1), 1.f);
 }
