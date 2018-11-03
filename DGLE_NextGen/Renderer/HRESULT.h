913c5f88a5c87a94fdf3a6abaccbe855868c92db
ash3D <alexey.shaydurov@deeprosoft.com>
2018-11-04 02:30:41 +0500

    HRESULT wrapped with _com_error class.

--- DGLE_NextGen/Renderer/HRESULT.h
+++ DGLE_NextGen/Renderer/HRESULT.h
@@ -1,6 +1,6 @@
 /**
 \author		Alexey Shaydurov aka ASH
-\date		22.07.2017 (c)Korotkov Andrey
+\date		04.11.2018 (c)Korotkov Andrey
 
 This file is a part of DGLE project and is distributed
 under the terms of the GNU Lesser General Public License.
@@ -10,9 +10,10 @@ See "DGLE.h" for more details.
 #pragma once
 
 #include <winerror.h>
+#include <comdef.h>
 
 inline void CheckHR(HRESULT hr)
 {
 	if (FAILED(hr))
-		throw hr;
+		throw _com_error(hr);
 }
