58bda8075bcc57392bb8d9adde56ed9e56138199
ash3D <alexey.shaydurov@deeprosoft.com>
2018-11-21 02:46:58 +0500

    D3D texture object naming.

--- Renderer/texture.cpp
+++ Renderer/texture.cpp
@@ -9,7 +9,10 @@ using WRL::ComPtr;
 Impl::Texture::Texture(const wchar_t *fileName)
 {
 	extern ComPtr<ID3D12Device2> device;
+	void NameObject(ID3D12Object *object, LPCWSTR name) noexcept;
+
 	unique_ptr<uint8_t []> data;
 	vector<D3D12_SUBRESOURCE_DATA> subresources;
 	CheckHR(DirectX::LoadDDSTextureFromFile(device.Get(), fileName, tex.GetAddressOf(), data, subresources));
+	NameObject(tex.Get(), fileName);
 }
