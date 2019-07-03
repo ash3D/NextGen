109e9e0f6b2b7effbef2f4d61b016aaa415cfc52
ash3D <alexey.shaydurov@deeprosoft.com>
2019-07-04 01:46:42 +0500

    Alphatest shader fixed, workaround for SM 6.0.

--- Renderer/object3DAlphatest_PS.hlsl
+++ Renderer/object3DAlphatest_PS.hlsl
@@ -16,19 +16,27 @@ ConstantBuffer<TonemapParams> tonemapParams : register(b1, space1);
 
 static const float alphaThreshold = .5f;
 
+#if 1
+// workaround for SM 6.0
+float4 main(in XformedVertex_UV input, in bool front : SV_IsFrontFace, out uint sampleMask : SV_Coverage, in uint coverageMask : SV_Coverage) : SV_TARGET
+{
+	sampleMask = coverageMask;
+#else
 float4 main(in XformedVertex_UV input, in bool front : SV_IsFrontFace, inout uint sampleMask : SV_Coverage) : SV_TARGET
 {
+#endif
 	input.N = normalize(front ? +input.N : -input.N);	// handles two-sided materials
 	input.viewDir = normalize(input.viewDir);
 	FixNormal(input.N, input.viewDir);
 
 	float3 albedo = 0;
-	// unrolled loop with known sample count could be faster
+	// unrolled loop with known sample count could be faster\
+	it's also unclear how compiler handles divergent tex lookup without loop unrolling
 	for (uint i = 0, curSampleBit = 1; curSampleBit <= sampleMask; i++, curSampleBit <<= 1u)
 	{
 		if (sampleMask & curSampleBit)
 		{
-			const float4 curSampleFetch = SelectTexture(ALBEDO_MAP).Sample(obj3DAlbedoSampler, EvaluateAttributeAtSample(input.uv, i));
+			const float4 curSampleFetch = SelectTexture(ALBEDO_MAP).Sample(obj3DAlbedoAlphatestSampler, EvaluateAttributeAtSample(input.uv, i));
 			if (curSampleFetch.a >= alphaThreshold)
 				albedo += curSampleFetch;
 			else
