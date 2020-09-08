cce01d40f7c42da0ae5edf82d40e2bd0202d62db
ash <alexey.shaydurov@deeprosoft.com>
2020-09-09 02:58:07 +0500

    DOF: bokeh mirroring for foreground field.

--- Renderer/DOF_splattingGS.hlsl
+++ Renderer/DOF_splattingGS.hlsl
@@ -61,7 +61,7 @@ void main(point DOF::SplatPoint splatPoint[1], inout TriangleStream<DOF::SpriteV
 			smoothstep(-DOF::layerBlendRange, +DOF::layerBlendRange, splatPoint[0].coc[0] - DOF::layerSeparationCoC * sign(splatPoint[0].coc[0])),	// blend far
 			splatPoint[0].col,
 			DOF::OpacityHalfres(splatPoint[0].coc[1], splatPoint[0].apt),
-			splatPoint[0].ext,
+			splatPoint[0].ext * sign(splatPoint[0].coc[0])/*mirror bokeh for foreground field*/,
 			(splatPoint[0].coc[1] + .5f) / Bokeh::R/*treat inner R as CoC, blow quad to fit*/,
 			splatPoint[0].rot,
 			max(splatPoint[0].coc[1] / splatPoint[0].apt + .5f, 1),
